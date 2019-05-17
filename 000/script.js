90b9ead75144d9a9bec7ee2b8e47fd916c93a975
taminaga0310 <taminaga0310>
2019-05-17 11:19:39 +0900

    コメント

---
+++ 000/script.js
@@ -0,0 +1,40 @@
+function menseki(haike,haikei,enn){
+  return"面積は"+haike*haikei*enn+"cmです。";
+}
+
+document.write(menseki(5,5,3.14));
+document.write("<br>");
+document.write(menseki(7,7,3.14));
+document.write("<br>");
+document.write(menseki(10,10,3.14));
+
+document.write("<br>");
+document.write("<br>");
+
+
+function hito(A,B){
+    return (A*500)+(B*200);
+}
+document.write(hito(2,4)+"円"+"<br>");
+document.write(hito(1,5)+"円"+"<br>");
+document.write(hito(3,7)+"円"+"<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
